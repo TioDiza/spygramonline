573080d52c9eda85e8b12e2e806c36b2a9eff64d
[dyad] <git@dyad.sh>
2025-10-05 18:41:29 -0300

    [dyad] wrote 2 file(s), deleted 1 file(s)

--- src/components/LoadingSequence.tsx
+++ src/components/LoadingSequence.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ChipLoader from './ChipLoader'; // Importa o novo ChipLoader
+import CircuitLoader from './CircuitLoader'; // Importa o novo CircuitLoader
 import HackingMessages from './HackingMessages'; // Importa o novo componente de mensagens
 
 interface LoadingSequenceProps {
@@ -9,8 +9,8 @@ interface LoadingSequenceProps {
 const LoadingSequence: React.FC<LoadingSequenceProps> = ({ onSequenceComplete }) => {
   return (
     <div className="flex flex-col items-center justify-center p-8 text-center min-h-[25rem]"> {/* Aumentado min-h para acomodar loader e mensagens */}
-      <div className="mb-4"> {/* Container para o ChipLoader */}
-        <ChipLoader />
+      <div className="mb-4"> {/* Container para o CircuitLoader */}
+        <CircuitLoader />
       </div>
       <HackingMessages onComplete={onSequenceComplete} />
     </div>
