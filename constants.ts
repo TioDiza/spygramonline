3ab73dda398c6c3c10c68f026b469ddb83b502d2
[dyad] <git@dyad.sh>
2025-10-05 21:22:26 -0300

    [dyad] wrote 1 file(s)

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE = 'https://www.spygram.services/apis';
 export const PROXY_FOLLOWERS_URL = `${API_BASE}/proxy_oraculo.php`;
-export const MIN_LOADING_DURATION = 50000; // 50 segundos, ajustado para a nova velocidade de digitação
+export const MIN_LOADING_DURATION = 3000; // 3 segundos, ajustado para uma experiência mais fluida
