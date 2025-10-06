ad9e4cd0c8c8c6da9d6c09576a977a96e5fc79f5
[dyad] <git@dyad.sh>
2025-10-06 10:19:50 -0300

    [dyad] Adicionando medidas anti-clone ao site - wrote 2 file(s)

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,6 +6,11 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+
+// Anti-clone: Desabilita o menu de contexto (clique direito)
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+});
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
