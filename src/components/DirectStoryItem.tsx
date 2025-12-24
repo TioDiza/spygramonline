0686ec1a491e68f6e34c8cf3c11d23970e8404ca
[dyad] <mouragabriel2011@gmail.com>
2025-12-23 21:25:01 -0300

    Reverted all changes back to version f1b8cef20bf6854b927de7d69f7c9954f6deab80

--- src/components/DirectStoryItem.tsx
+++ src/components/DirectStoryItem.tsx
@@ -10,7 +10,7 @@ interface DirectStoryItemProps {
 const DirectStoryItem: React.FC<DirectStoryItemProps> = ({ avatarUrl, name, note, isOwnStory = false }) => {
   return (
     <div className="story-item">
-      <div className="story-bubble">{note}</div>
+      {!isOwnStory && <div className="story-bubble">{note}</div>}
       <img src={avatarUrl} alt={name} className="story-avatar" />
       <span className="story-name">{isOwnStory ? 'Sua nota' : name}</span>
     </div>
